117de437e1d988b6cb32e0ed175d5a0278124190
IcamyS <105745467+icamysss@users.noreply.github.com>
2025-02-21 19:26:52 +0300

    empty

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+namespace PeakTrials.Scripts.Common
+{
+    public class Singleton<T> : MonoBehaviour where T : Component
+    {
+        private static T _instance;
+
+        private static bool HasInstance => _instance != null;
+        public static T TryGetInstance() => HasInstance ? _instance : null;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+                
+                _instance = FindAnyObjectByType<T>();
+                if (_instance != null) return _instance;
+                
+                var go = new GameObject(typeof(T).Name + " Auto-Generated");
+                _instance = go.AddComponent<T>();
+
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// Убедится в вызове base.Awake() при переопределении, если нужен Awake().
+        /// </summary>
+        protected virtual void Awake()
+        {
+            InitializeSingleton();
+        }
+
+        protected virtual void InitializeSingleton()
+        {
+            if (!Application.isPlaying) return;
+            
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                if (_instance != this)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
