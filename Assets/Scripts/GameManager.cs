b6595874ee6c0594dd22aaf900958473e5bfa19e
IcamyS <105745467+icamysss@users.noreply.github.com>
2025-03-01 22:12:48 +0300

    ADD UI
    Добавил набросок ui

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,22 @@
 using System;
+using DefaultNamespace;
 using UnityEngine;
 
-public enum PlayerType { Human, AI }
+public enum GameType { HvsH, HvsAI, AIvsAI} 
 
 public class GameManager: Singleton<GameManager>
 {
     public static GameManager Instance;
 
     [SerializeField] private Board boardPrefab;
-    [SerializeField] private AIController aiController;
-    
+    [SerializeField] private AIController aiControllerPrefab;
+    [SerializeField] private UIManager uiManagerPrefab;
 
-   
+
+    public void StartGame(GameType gameType)
+    {
+        throw new NotImplementedException();
+    }
 
    
 }
