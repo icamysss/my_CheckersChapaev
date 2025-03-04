fb24d0721015a26a9ca1e136c1a4f18b4cf15aaa
IcamyS <105745467+icamysss@users.noreply.github.com>
2025-03-04 21:40:50 +0300

    InGame
    Отображение информации игры

--- Assets/Scripts/UI/InGame.cs
+++ Assets/Scripts/UI/InGame.cs
@@ -1,7 +1,79 @@
+using System;
+using Core;
+using Services;
+using UnityEngine;
+using UnityEngine.UI;
+
 namespace UI
 {
     public class InGame : Menu
     {
+        [SerializeField] private Text whiteCount, blackCount;
+        [SerializeField] private Text whoTurn;
         
+        private Game game;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            game = ServiceLocator.Get<IGameManager>().CurrentGame;
+
+            game.OnEndMove += UpdateUI;
+            game.OnGameStart += UpdateUI;
+            game.OnGameEnd += OnGameEnded;
+            
+
+            whiteCount.text = string.Empty;
+            blackCount.text = string.Empty;
+            whoTurn.text = string.Empty;
+        }
+
+        private void UpdateUI()
+        {
+            whiteCount.text = game.Board.GetPawnsOnBoard(PawnColor.White).Count.ToString();
+            blackCount.text = game.Board.GetPawnsOnBoard(PawnColor.Black).Count.ToString();
+
+            switch (game.CurrentTurn)
+            {
+                case PawnColor.None:
+                    whoTurn.text = "Выберите шашку для хода";
+                    break;
+                case PawnColor.Black:
+                    whoTurn.text = "Ходят черные";
+                    break;
+                case PawnColor.White:
+                    whoTurn.text = "Ходят белые";
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            ;
+        }
+
+        private void OnGameEnded(PawnColor winColor)
+        {
+            UpdateUI();
+            switch (winColor)
+            {
+                case PawnColor.None:
+                    whoTurn.text = "Ничья";
+                    break;
+                case PawnColor.Black:
+                    whoTurn.text = "Черные победили";
+                    break;
+                case PawnColor.White:
+                    whoTurn.text = "Белые победили";
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(winColor), winColor, null);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            game.OnEndMove -= UpdateUI;
+            game.OnGameStart -= UpdateUI;
+            game.OnGameEnd -= OnGameEnded;
+        }
     }
 }
