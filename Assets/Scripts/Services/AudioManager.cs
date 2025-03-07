f5eadc9a1b3100a9bbee4696ae1629155d6672cd
IcamyS <105745467+icamysss@users.noreply.github.com>
2025-03-07 17:42:42 +0300

    Update AudioManager.cs
    Упростил немного

--- Assets/Scripts/Services/AudioManager.cs
+++ Assets/Scripts/Services/AudioManager.cs
@@ -6,8 +6,7 @@ namespace Services
     public class AudioManager : MonoBehaviour, IAudioService
     {
         [SerializeField] private PawnAudio pawnAudio;      // Клипы для шашек
-        [SerializeField] private AudioSource musicSource;  // Источник для фоновой музыки
-        [SerializeField] private AudioSource sfxSource;    // Источник для звуков эффектов
+        [SerializeField] private float volume; 
 
         private AudioSource audioSource;
         
@@ -19,6 +18,7 @@ namespace Services
             Debug.Log("Audio Manager initialized");
             audioSource = GetComponent<AudioSource>();
             pawnAudio.Initialize(audioSource);
+            // todo загрузить громкость
             isInitialized = true;
         }
 
@@ -34,33 +34,26 @@ namespace Services
         #region IAudioService
 
         // Установка общей громкости
-        public void SetVolume(float volume)
+        public float Volume
         {
-            musicSource.volume = volume;
-            sfxSource.volume = volume;
+            get => volume;
+            set
+            {
+                value = Mathf.Clamp(value, 0, 1);
+                audioSource.volume = volume;
+                volume = value;
+                
+                // todo сохранить громкость
+            }
         }
-
+        
         // Доступ к клипам шашек
         public PawnAudio PawnAudio => pawnAudio;
-
-        // Воспроизведение фоновой музыки
-        public void PlayMusic(AudioClip clip)
-        {
-            musicSource.clip = clip;
-            musicSource.loop = true;
-            musicSource.Play();
-        }
-
-        // Остановка музыки
-        public void StopMusic()
-        {
-            musicSource.Stop();
-        }
-
+        
         // Воспроизведение однократного звука (например, клик в меню)
         public void PlaySound(AudioClip clip)
         {
-            sfxSource.PlayOneShot(clip);
+            audioSource.PlayOneShot(clip);
         }
 
         #endregion
