ee22766cc3689a894878707e2b983f5c9e8ba79d
IcamyS <105745467+icamysss@users.noreply.github.com>
2025-03-07 17:42:57 +0300

    Update IAudioService.cs
    метод на свойство поменял

--- Assets/Scripts/Services/Interfaces/IAudioService.cs
+++ Assets/Scripts/Services/Interfaces/IAudioService.cs
@@ -8,22 +8,13 @@ namespace Services
         /// Устанавливает громкость
         /// </summary>
         /// <param name="volume">float от 0 до 1</param>
-        void SetVolume(float volume); // 0.....1
+        float Volume { get; set; } // 0.....1
         
         /// <summary>
         /// Доступ к клипам звуков шашек
         /// </summary>
         PawnAudio PawnAudio { get; }
         
-        /// <summary>
-        /// Воспроизводит фоновую музыку
-        /// </summary>
-        void PlayMusic(AudioClip clip);
-        
-        /// <summary>
-        /// Останавливает фоновую музыку
-        /// </summary>
-        void StopMusic();
         
         /// <summary>
         /// Воспроизводит однократный звук эффекта
