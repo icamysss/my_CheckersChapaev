8829ba995bddfc0b3e22d1b04432b154085001e8
IcamyS <105745467+icamysss@users.noreply.github.com>
2025-03-17 21:22:07 +0300

    Update AIController.cs
    Более читабельно

--- Assets/Scripts/AI/AIController.cs
+++ Assets/Scripts/AI/AIController.cs
@@ -53,31 +53,55 @@ namespace AI
         /// </summary>
         public async UniTask MakeMove(Player pl, CancellationToken cancellationToken)
         {
-            if (pl.PawnColor == PawnColor.None)
+            if (pl == null || pl.PawnColor == PawnColor.None )
             {
-                Debug.LogError($"Invalid pawn color: {pl.PawnColor}");
+                Debug.LogError("Invalid player, cant make move");
                 return;
             }
 
-            
-            
-            aiSettings = pl.AISettings ?? new AISettings();
-            
-            RefreshPawnLists(pl.PawnColor);
-            aiCalculator = new AICalculator(pl, board, aiPawns, enemyPawns);
-
             try
             {
-                if (cancellationToken.IsCancellationRequested) return;
-                await AIMove(cancellationToken);
-            }
-            catch (OperationCanceledException)
-            {
-                Debug.Log("AI move was cancelled.");
+                // 1. ========= настройка параметров хода =========
+                RefreshPawnLists(pl.PawnColor);
+                aiCalculator = new AICalculator(pl, board, aiPawns, enemyPawns);
+                aiSettings = pl.AISettings ?? new AISettings(); // если настроек ии нет, создаем новые
+                aiSelectedPawn = aiCalculator.SelectOptimalPawn();
+                shotDirection = aiCalculator.CalculateOptimalDirection(aiSelectedPawn);
+                shotPower = aiCalculator.CalculateForce(aiSelectedPawn);
             }
             catch (Exception ex)
             {
                 Debug.LogWarning($"AI Move failed: {ex}");
+                // вызываем событие для смены хода
+                Pawn.OnKickPawn?.Invoke();
+            }
+            
+            try
+            {
+                // 2. ========= Задержка для имитации "размышлений" ИИ =========
+                var decisionDelay = Random.Range(aiSettings.MinDecisionDelayMS, aiSettings.MaxDecisionDelayMS);
+                await UniTask.Delay(decisionDelay, cancellationToken: cancellationToken);
+                aiSelectedPawn.Select(); // выбор шашки, после выбора камера передвигается к выбранной шашке
+
+                // 3. ========= Ожидание завершения движения камеры =========
+                var cameraMoveDelay = cameraController.MoveDurationMS + aiSettings.TimeAfterCamSetPositionMS;
+                await UniTask.Delay(cameraMoveDelay, cancellationToken: cancellationToken);
+
+                // 4. ========== Имитация прицеливания ================
+                await AimSimulateAsync(cancellationToken);
+            }
+            catch (OperationCanceledException)
+            {
+                Debug.Log("AI move was cancelled. Simulation failed");
+            }
+            catch (Exception e)
+            {
+                Debug.LogWarning($"AI Move simulation failed: {e}");
+            }
+            finally
+            {
+                // 5. ========== Применение силы для выполнения хода ============
+                aiSelectedPawn.ApplyForce(shotDirection * shotPower);
             }
         }
 
@@ -94,35 +118,6 @@ namespace AI
             enemyPawns = board.GetPawnsOnBoard(
                 pawnColor == PawnColor.Black ? PawnColor.White : PawnColor.Black
             ).Where(p => p != null).ToList();
-        }
-
-        /// <summary>
-        /// Основной метод хода ИИ с поддержкой отмены
-        /// </summary>
-        private async UniTask AIMove(CancellationToken cancellationToken)
-        {
-            // 1. ========= Задержка для имитации "размышлений" ИИ =========
-            var decisionDelay = Random.Range(aiSettings.MinDecisionDelayMS, aiSettings.MaxDecisionDelayMS);
-            await UniTask.Delay(decisionDelay, cancellationToken: cancellationToken);
-
-            // 2. ========= Выбор оптимальной шашки и ее активация =========
-            aiSelectedPawn = aiCalculator.SelectOptimalPawn();
-            if (aiSelectedPawn == null)
-                throw new Exception("No valid pawn selected for AI move.");
-            aiSelectedPawn.Select();
-            
-            shotDirection = aiCalculator.CalculateOptimalDirection(aiSelectedPawn);
-            shotPower = aiCalculator.CalculateForce(aiSelectedPawn);
-            
-            // 3. ========= Ожидание завершения движения камеры =========
-            var cameraMoveDelay = cameraController.MoveDurationMS + aiSettings.TimeAfterCamSetPositionMS;
-            await UniTask.Delay(cameraMoveDelay, cancellationToken: cancellationToken);
-
-            // 4. ========== Имитация прицеливания ================
-            await AimSimulateAsync(cancellationToken);
-
-            // 5. ========== Применение силы для выполнения хода ============
-            aiSelectedPawn.ApplyForce(shotDirection * shotPower);
         }
         
         /// <summary>
